36daa2e5da5e64451c2c14c66bc96bed28698eda
MouradM <wassakh@gmail.com>
2022-02-22 09:40:33 +0100

    Updates

---
+++ precache-manifest.f62e8c2f505f53bcd859ccfed91ec0a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6116790feec5ed12cfa36f1562af61",
+    "url": "/VotingSystem/index.html"
+  },
+  {
+    "revision": "b3f8501af8eabaa575f5",
+    "url": "/VotingSystem/static/css/2.37c457eb.chunk.css"
+  },
+  {
+    "revision": "5810bf75b318a9f52d19",
+    "url": "/VotingSystem/static/css/main.b1982e4d.chunk.css"
+  },
+  {
+    "revision": "b3f8501af8eabaa575f5",
+    "url": "/VotingSystem/static/js/2.60293c79.chunk.js"
+  },
+  {
+    "revision": "5810bf75b318a9f52d19",
+    "url": "/VotingSystem/static/js/main.cc849ba3.chunk.js"
+  },
+  {
+    "revision": "bf1cb66f6f775c512ea5",
+    "url": "/VotingSystem/static/js/runtime-main.b52954d3.js"
+  }
+]);
